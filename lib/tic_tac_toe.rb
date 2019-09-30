28abf1a641688603386915cb400be6763a3aabc2
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-30 18:58:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,7 +66,6 @@ def won?
     @board[combo[0]] == @board[combo[1]] &&
     @board[combo[1]] == @board[combo[2]] &&
     position_taken?(@board, combo[0])
-    puts @board[combo.first]
   end
 
 end
