f3b3864e1162ab0787c0fee24473c86d6cacfdc1
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-30 19:37:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,4 +87,13 @@ class TicTacToe
     end
   end
 
+  def play
+  turn until over?
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
 end
