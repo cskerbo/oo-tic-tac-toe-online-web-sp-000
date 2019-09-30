3101d8276debed9d047349197e53123e07db279f
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-30 18:32:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,8 +63,8 @@ end
 
 def won?
   WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == @board[combo[1]] &&
-      board[combo[1]] == @board[combo[2]] &&
+    @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
       position_taken?(@board, combo[0])
   end
 end
